6cda3f49c3870a65ca6af4d5ed943f299abe365c
d0220759 <91425671+d0220759@users.noreply.github.com>
2021-09-29 19:17:20 +0100

    Add files via upload

---
+++ gd3GettingStartedd/gd3GettingStartedd/MathUtility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GDLibrary.Utility
+{
+    /// <summary>
+    /// Provide math functions for use within our game 
+    /// </summary>
+    /// <see cref="GBLibrary.Program"/>
+    /// <seealso cref="GDLibrary.Program"/>
+    /// <exaple>
+    /// int rand = MathUtility.RandInRange(1,100>;
+    /// </exaple>
+    class MathUtility
+    {
+        /// <summary>
+        /// Returns a int in the range lo-hi 
+        /// </summary>
+        /// <param name="lo"> Integer value <=hi</param>
+        /// <param name="hi">Integer value >= lo</param>
+        /// <returns>Random integer</returns>
+        public static int RandInRange(int lo, int hi)
+        {
+            if (hi < lo)
+                throw new Exception("hi must be >= to lo");
+
+
+
+
+            //System.Random rand = new System.Random();
+            Random rand = new Random();
+            //int x=(hi - lo) * rand.Next() + lo;
+
+            return rand.Next(lo, hi);
+        }
+
+        //RandInRangeExcl (int lo, int hi, int exclusion
+        //excluse a number 
+        public static int RandInRangeExclude(int lo, int hi, int exclusion)
+        {
+            if (hi < lo)
+                throw new Exception("hi must be >= to lo");
+        }
+    }
+}
---
+++ gd3GettingStartedd/gd3GettingStartedd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using GDLibrary.Utility;
+
+namespace GDLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Start();
+        }
+
+        void Start() // non static or instance method
+        {
+            #region Print Demo
+            // value types(passed by copy)
+            int x = 10;
+            double y = 20;
+            Print(x, y);
+            Console.WriteLine($"{x}and{y}");
+
+            #endregion 
+
+            for (int i = 1; i <= 30; i++)
+            {
+                int randNum = MathUtility.RandInRange(5, 10);
+                Console.WriteLine(randNum);
+            }
+        }
+
+        void Print(int a, double b)
+        {
+            // Console.WriteLine(a + "and" + b);
+            a = -10;
+            b = 1E6;
+
+            Console.WriteLine($"{a}and{b}");
+        }
+
+    }
+}
